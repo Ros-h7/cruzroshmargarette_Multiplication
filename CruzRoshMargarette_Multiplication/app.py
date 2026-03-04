9fc5c35a7a00554f5b0aa3bc42a39a05c254213c
Ros-h7 <cruzmargarette3@gmail.com>
2026-03-04 10:15:07 +0800

    Add files via upload

---
+++ CruzRoshMargarette_Multiplication/app.py
@@ -0,0 +1,8 @@
+print("------------------------------")
+print(" MULTIPLICATION CALCULATOR ")
+print("------------------------------")
+CruzNum1 = int(input("Enter first number: "))
+CruzNum2 = int(input("Enter second number: "))
+CruzProduct = CruzNum1 * CruzNum2
+print(f"Answer: {CruzProduct}")
+
